bbdfa64aa13cdd47a3c0b00638947cea6dd3bae5
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-08-23 12:45:59 -0400

    got my api connected git push origin master '

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,30 @@
-/* api key for rawg.io: e0bf16b64b704c9ca38eaac77d4a5087
-next step is using ajax to figure out how to connect it to my project using jquery and ajax`
-*/
+const URL = "https://api.rawg.io/api/platforms?key=e0bf16b64b704c9ca38eaac77d4a5087"
 
-/*GET URL="https://api.rawg.io/api/platforms?key=e0bf16b64b704c9ca38eaac77d4a5087"
-GET URL="https://api.rawg.io/api/games?key=e0bf16b64b704c9ca38eaac77d4a5087&dates=2019-09-01,2019-09-30&platforms=18,1,7" */
+const $name = $('#title');
+const $textInput = $('#textInput');
+const $gamesCount = $('#gamesCount');
 
-//$.get("https://api.rawg.io/api/platforms?key=e0bf16b64b704c9ca38eaac77d4a5087")
+
+
+$.ajax(URL)
+  .then((data)=>{
+     console.log(data)
+})
+
+
+// $textInput.on('submit', getGameData)
+// //let gameData;
+
+// function getGameData(event){
+//     event.preventDefault()
+//     const userInput = $input.val();
+//     if (userInput === '') return;
+    
+//     });
+    
+// }
+
+
+// // function render(){
+    
+// // }
