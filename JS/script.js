8856b8f1ce0e6c78ef48811cc311dc7df541f31d
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-08-23 00:15:38 -0400

    navbar finished git add

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,5 @@ next step is using ajax to figure out how to connect it to my project using jque
 
 /*GET URL="https://api.rawg.io/api/platforms?key=e0bf16b64b704c9ca38eaac77d4a5087"
 GET URL="https://api.rawg.io/api/games?key=e0bf16b64b704c9ca38eaac77d4a5087&dates=2019-09-01,2019-09-30&platforms=18,1,7" */
+
+//$.get("https://api.rawg.io/api/platforms?key=e0bf16b64b704c9ca38eaac77d4a5087")
